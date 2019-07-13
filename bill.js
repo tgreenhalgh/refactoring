bc975a677d697bf14deb792fa9b67f1a19f0aa17
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-07-12 17:41:21 -0700

    inline variable

--- bill.js
+++ bill.js
@@ -76,8 +76,8 @@ function statement(invoice, plays) {
       volumeCredits += Math.floor(perf.audience / 5);
     // print line for this order
     /* prettier-ignore */
-    result += `   ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += `   ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
