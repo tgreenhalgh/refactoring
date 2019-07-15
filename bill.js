4d52ed28f75567d6306933c371c9146530f5b128
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-07-15 15:22:36 -0700

    function to calculate volumeCredits

--- bill.js
+++ bill.js
@@ -84,6 +84,14 @@ function statement(invoice, plays) {
   return result;
 }
 
+function volumeCreditsFor(perf) {
+  let volumeCredits = 0;
+  volumeCredits += Math.max(perf.audience - 30, 0);
+  if ('comedy' === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+  return volumeCredits;
+}
+
 // console.log(statement(invoice[0], plays));
 
 module.exports = {
